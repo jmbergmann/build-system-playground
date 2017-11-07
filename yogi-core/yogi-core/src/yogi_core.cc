8e27c2f1cb989a7ea3f47b0d0f5a8e075c30586f
Johannes M Bergmann <JMBergmann@dynamiccontrols.com>
2017-11-08 08:56:30 +1300

    .

---
+++ yogi-core/yogi-core/src/yogi_core.cc
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "../../include/yogi_core.h"
+
+
+YOGI_API const char* YOGI_GetVersion()
+{
+  return YOGI_VERSION;
+}
--- yogi-core/yogi-core/yogi_core.cc
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "../include/yogi_core.h"
-
-
-YOGI_API const char* YOGI_GetVersion()
-{
-  return YOGI_VERSION;
-}
