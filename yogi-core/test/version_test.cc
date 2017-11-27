f5a846910046ae9a32418ea76215a6a4d3cb7bcc
Johannes Bergmann <mail@johannes-bergmann.de>
2017-11-27 20:52:37 +1300

    .

--- yogi-core/test/version_test.cc
+++ yogi-core/test/version_test.cc
@@ -2,6 +2,18 @@
 
 #include <yogi_core.h>
 
+TEST(VersionTest, Macros) {
+	std::ostringstream ss;
+	ss << YOGI_VERSION_MAJOR << '.' << YOGI_VERSION_MINOR << '.' << YOGI_VERSION_PATCH;
+
+	if (strlen(YOGI_VERSION_SUFFIX)) {
+		ss << '-' << YOGI_VERSION_SUFFIX;
+	}
+
+	auto version = ss.str();
+	EXPECT_EQ(version, YOGI_VERSION);
+}
+
 TEST(VersionTest, GetVersion) {
-  EXPECT_STREQ("0.0.3-alpha", YOGI_GetVersion());
+  EXPECT_STREQ(YOGI_VERSION, YOGI_GetVersion());
 }
