ebab8e8ab1327aee21313be8b05a59c9a9c177a2
Johannes Bergmann <community@johannes-bergmann.de>
2017-12-06 19:41:52 +1300

    Removed yogi-python

--- yogi-python/test/test_free_functions.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import yogi
-
-class Test_test1(unittest.TestCase):
-    def test_A(self):
-        self.assertEqual(yogi.get_version(), '0.0.3-alpha')
-
-if __name__ == '__main__':
-    unittest.main()
