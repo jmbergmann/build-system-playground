42b8498882c0145f3a79dd4e85ea8c1ee5dc96ca
Johannes Bergmann <mail@johannes-bergmann.de>
2018-01-05 19:24:41 +1300

    .

--- yogi-dotnet/yogi/Library.cs
+++ yogi-dotnet/yogi/Library.cs
@@ -3,11 +3,11 @@ using System.Runtime.InteropServices;
 
 namespace Yogi
 {
-    public class Library
-    {
-        [DllImport("yogi-core.dll")]
-        static extern IntPtr YOGI_GetVersion();
+  public class Library
+  {
+    [DllImport("yogi-core.dll")]
+    static extern IntPtr YOGI_GetVersion();
 
-        public static string GetVersion() => Marshal.PtrToStringAnsi(YOGI_GetVersion());
-    }
+    public static string GetVersion() => Marshal.PtrToStringAnsi(YOGI_GetVersion());
+  }
 }
