be8a594128dc3cead649a186af21bd3d0065577f
Johannes Bergmann <mail@johannes-bergmann.de>
2018-01-18 08:34:44 +1300

    Using dotnet-cli for yogi-dotnet

--- yogi-dotnet/test/LibraryTest.cs
+++ yogi-dotnet/test/LibraryTest.cs
@@ -1,22 +1,24 @@
-﻿using NUnit.Framework;
 using System;
 using System.Reflection;
+using Xunit;
 using Yogi;
 
 namespace test
 {
-  [TestFixture]
   public class LibraryTest
   {
-    [Test]
-    public void Version() => Assert.IsNotEmpty(Yogi.Library.GetVersion());
+    [Fact]
+    public void Version()
+    {
+      Assert.True(Yogi.Library.GetVersion().Length > 3);
+    }
 
-    [Test]
+    [Fact]
     public void AssemblyVersion()
     {
       var v = Assembly.GetAssembly(typeof(Yogi.Library)).GetName().Version;
       var s = string.Format("{0}.{1}.{2}", v.Major, v.Minor, v.Build);
-      Assert.AreEqual(Yogi.Library.GetVersion(), s);
+      Assert.Equal(Yogi.Library.GetVersion(), s);
     }
   }
 }
