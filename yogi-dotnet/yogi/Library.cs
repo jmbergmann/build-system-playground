21bebc713dc3c5f6b89612fd8c9c5cca0e5b62f7
Johannes Bergmann <mail@johannes-bergmann.de>
2018-01-18 08:59:02 +1300

    Tried to make loading yogi-core.dll dynamically but need dlopen on Linux :(

--- yogi-dotnet/yogi/Library.cs
+++ yogi-dotnet/yogi/Library.cs
@@ -5,8 +5,33 @@ namespace Yogi
 {
   public class Library
   {
-    [DllImport("yogi-core.dll")]
-    static extern IntPtr YOGI_GetVersion();
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr LoadLibrary(string dllToLoad);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeLibrary(IntPtr hModule);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate IntPtr GetVersionDelegate();
+    private static GetVersionDelegate YOGI_GetVersion;
+
+    static Library()
+    {
+      IntPtr dll = LoadLibrary("yogi-core.dll");
+      if (dll == IntPtr.Zero) {
+        // TODO: error handling
+      }
+
+      IntPtr fn = GetProcAddress(dll, "YOGI_GetVersion");
+      if (fn == IntPtr.Zero) {
+        // TODO: error handling
+      }
+
+      YOGI_GetVersion = (GetVersionDelegate)Marshal.GetDelegateForFunctionPointer(fn, typeof(GetVersionDelegate));
+    }
 
     public static string GetVersion() => Marshal.PtrToStringAnsi(YOGI_GetVersion());
   }
