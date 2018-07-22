6672d82bb8ff4e5bcac1de2aaad671fb7f8033fd
Johannes Bergmann <mail@johannes-bergmann.de>
2018-07-22 17:52:23 +0200

    .

--- yogi-cpp/include/yogi/time.h
+++ yogi-cpp/include/yogi/time.h
@@ -10,11 +10,9 @@ namespace yogi {
 
 YOGI_DEFINE_API_FN(int, YOGI_GetCurrentTime, (long long* timestamp))
 
-/***************************************************************************//**
- * Get the current time.
- *
- * \returns The current time since 01/01/1970 UTC.
- ******************************************************************************/
+//! Get the current time.
+//!
+//! \returns The current time since 01/01/1970 UTC.
 inline std::chrono::system_clock::time_point GetCurrentTime() {
   long long timestamp;
   int res = internal::YOGI_GetCurrentTime(&timestamp);
